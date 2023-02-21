5fb78f157545539b63fd04ad79036640e2ccc37f
mehedi1256 <mehedihassanshovospirt@gmail.com>
2023-02-21 13:23:57 +0600

    3rd commit

--- js/jquery.sticky.js
+++ js/jquery.sticky.js
@@ -4,7 +4,8 @@
 // Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
 // Improvements by Leonardo C. Daronco (daronco)
 // Created: 02/14/2011
-// Date: 07/20/2015
+// Date: 07/20/2022
+    
 // Website: http://stickyjs.com/
 // Description: Makes an element on the page stick on the screen as you scroll
 //              It will only set the 'top' and 'position' of your element, you
